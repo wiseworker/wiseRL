8ed9dc70c9977c8d746361399f6e49f4e7cfac02
Nyrus-Y <75808398+Nyrus-Y@users.noreply.github.com>
2023-09-22 02:21:59 +0800

    restruct

--- example/dqn/dqn.py
+++ example/dqn/dqn.py
@@ -1,26 +1,29 @@
 from wiserl.core.runner import Runner
 from wiserl.agent.dqn_agent import DQNAgent
 from wiserl.net.dqn_net import DQNNet
-from wiserl.core.wise_rl import  WiseRL
+from wiserl.core.wise_rl import WiseRL
 import gym
 import time
 import ray
+
 wise_rl = WiseRL()
 
+
 class GymRunner(Runner):
-    def __init__(self,local_rank=0):
+    def __init__(self, local_rank=0):
         self.env = gym.make("CartPole-v1")
-        print("rank=",local_rank)
+        print("rank=", local_rank)
         N_ACTIONS = self.env.action_space.n  # 2 actions
-        N_STATES = self.env.observation_space.shape[0] # 4 states
+        N_STATES = self.env.observation_space.shape[0]  # 4 states
         self.local_rank = local_rank
-        if local_rank ==0:
-            #net = DQNNet(N_STATES,N_ACTIONS)
-            wise_rl.makeAgent(name ="dqn_agent",agent_class=DQNAgent, net_class=DQNNet,n_states=N_STATES,n_actions=N_ACTIONS,sync=False)
+        if local_rank == 0:
+            # net = DQNNet(N_STATES,N_ACTIONS)
+            wise_rl.makeAgent(name="dqn_agent", agent_class=DQNAgent, net_class=DQNNet, n_states=N_STATES,
+                              n_actions=N_ACTIONS, sync=False)
             self.agent = wise_rl.getAgent("dqn_agent")
         else:
             self.agent = wise_rl.getAgent("dqn_agent")
-       
+
     def run(self):
         print("run")
         start = time.time()
@@ -39,11 +42,12 @@ class GymRunner(Runner):
                 if done:
                     r = -10
                     end = time.time()
-                    print(self.local_rank, 'time', round((end-start),2),' Ep: ', i_episode, ' |', 'Ep_r: ', round(ep_r, 2))
+                    print(self.local_rank, 'time', round((end - start), 2), ' Ep: ', i_episode, ' |', 'Ep_r: ',
+                          round(ep_r, 2))
                     break
-                s = s_  
+                s = s_
 
-if __name__=='__main__':
-   
-    runners = wise_rl.makeRunner("runner",GymRunner,num=5)
+
+if __name__ == '__main__':
+    runners = wise_rl.makeRunner("runner", GymRunner, num=5)
     wise_rl.startAllRunner(runners)
